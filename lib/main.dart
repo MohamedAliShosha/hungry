baf219b739e82cc78a7ea2508c61becb3680e3c7
Mohamed Ali <160479090+MohamedAliShosha@users.noreply.github.com>
2026-01-28 14:29:20 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Hungry());
+}
+
+class Hungry extends StatelessWidget {
+  const Hungry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
